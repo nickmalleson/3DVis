ab2b39258886751e13afb41932a961ab350db801
Nick Malleson <n.malleson06@leeds.ac.uk>
2014-05-16 17:08:39 +0100

    Made progress - now can convert between map and screen coordinates. Still doesn't work though, but hopefully closer. Also sorted out difference between Locations and PVectors so fewer problems about x,y lat,lon

--- SpaceTime/src/spacetime/Main.java
+++ SpaceTime/src/spacetime/Main.java
@@ -2,15 +2,9 @@ package spacetime;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.gicentre.utils.spatial.OSGB;
 import processing.core.*;
 import peasy.*;
 import de.fhpotsdam.unfolding.*;
@@ -20,27 +14,19 @@ import de.fhpotsdam.unfolding.geo.*;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.utils.*;
 import de.fhpotsdam.unfolding.providers.*;
+import java.util.Arrays;
 
 public class Main extends PApplet {
 
     PImage backgroundMap;        // OpenStreetMap.
-//    PVector tlCorner, brCorner;   // Corners of map in WebMercator coordinates.
-//    OSGB bng = new OSGB(); // For converting from lat/lon to bng
     
     // Corners of the data
     PVector tlCorner, brCorner;
-    //PVector tlCorner = bng.transformCoords(new PVector(-1.646491f, 53.868911f));
-    //PVector brCorner = bng.transformCoords(new PVector(-1.630948f, 53.843498f));
-    
-//    PVector centre; // Centre of the map
-//    PVector screenCentre; // Centre converted to screen pixels
+
     PeasyCam cam;
-//    List<Float> xcors = new ArrayList<Float>();
-//    List<Float> ycors = new ArrayList<Float>();
-    
-    //List<PVector> bngcoords = new ArrayList<PVector>();
+
     List<PVector> screenCoords = new ArrayList<PVector>();
-    List<PVector> latloncoords = new ArrayList<PVector>();
+    List<Location> latloncoords = new ArrayList<Location>();
     List<Float> times = new ArrayList<Float>();
     
     UnfoldingMap map;
@@ -56,20 +42,6 @@ public class Main extends PApplet {
         //Both P2D and P3D use openGL by default
         size(800, 800, OPENGL);
         
-        // Read the GPS data
-        try {
-            readData();
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-            ex.printStackTrace();
-        }
-
-        // FInd out where to look (centre of base map)
-//        centre = new PVector(brCorner.x - tlCorner.x, tlCorner.y - brCorner.x);
-//        screenCentre = geoToScreen(centre);
-//        assert centre.x > 0 && centre.y > 0;
-
-//        cam = new PeasyCam(this, screenCentre.x,screenCentre.y,0,1000);
         cam = new PeasyCam(this,
                 400,        // look at x
                 400,        // look at y
@@ -79,34 +51,49 @@ public class Main extends PApplet {
         cam.setMinimumDistance(10);
         cam.setMaximumDistance(1000);
         
-        // Set up the map. Note some confusion over x,y and lon,lat.
-        // PVectors have (x,y), but Locations have (y,x)
-        Location startLocation = new Location(
-                latloncoords.get(0).y,      // y position (lat)
-                latloncoords.get(0).x);     // x position (lon)
-        System.out.println("Sart location: "+startLocation.toString()+
-                " - ("+startLocation.x+","+startLocation.y+")");
+        /* *************Set up the map and read data. ***************
+        Order is important: the map needs to be initialised
+        first, so that I can compare between screen and map coordinates. Then data
+        needs to be read (so we know starting position)
+        Then map can be zoomed and panned.
+        *****************************/
         
         map = new UnfoldingMap(
                 this,
-                startLocation.x,        // x position (lon)
-                startLocation.y,        // y position (lat)
+                1,        // x position (lon) (these coords don't matter, pan later)
+                1,        // y position (lat)
                 800f,                         // width
                 800f,                         // height
                 new Microsoft.RoadProvider() // (see providers here: http://unfoldingmaps.org/tutorials/mapprovider-and-tiles.html#)
                 //new Google.GoogleMapProvider()
                 //new Microsoft.AerialProvider()
         );
+
+        // Read the GPS data
+        try {
+            readData();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            ex.printStackTrace();
+        }
+        // Now move the map. Note some confusion over x,y and lon,lat.
+        Location startLocation = latloncoords.get(0);
+
+        System.out.println("Sart location: "+startLocation.toString()+
+                " - ("+startLocation.x+","+startLocation.y+")");
         
-        map.zoomAndPanTo(startLocation, 10);
+        // Need to pan here because the constructor takes screen coordinates, but want to give latlon coords
+        map.zoomTo(15);
+        map.panTo(startLocation);
+        
         
         
         // Add markers
         try {
             MarkerFactory mf = new MarkerFactory();
             Marker m;
-            for (PVector p:latloncoords) {
-                m = mf.createMarker(new PointFeature(new Location(p.y,p.x)));
+            for (Location p:latloncoords) {
+                m = mf.createMarker(new PointFeature(p));
                 map.addMarker(m);
             }
         } catch (Exception e) {
@@ -118,32 +105,12 @@ public class Main extends PApplet {
         // when we try to zoom in and out of data
         //MapUtils.createDefaultEventDispatcher(this, map);
         
-//        sphereDetail(5);
-
-
-
     }//setup
 
     @Override
     public void draw() {
 
         background(255);
-
-
-        //fill(0);
-        //text("" + frameRate, 10, 10);
-        //noFill();
-
-        // Draw the rectangle
-//        pushMatrix();
-////        translate (screenCentre.x, screenCentre.y);
-//        translate(-1500, -1000);
-//        fill(100, 100, 100);
-//        noStroke();
-//        rect(0, 0, 3000, 2000);
-//        popMatrix();
-        
-        
 
         // start drawing the 3d track
         noFill();
@@ -160,24 +127,9 @@ public class Main extends PApplet {
         
         map.draw();
         
-//        for (int i=0; i<coords.size(); i++) {
-//            pushMatrix();
-//            translate(screenCoords.get(i).x, screenCoords.get(i).y, times.get(i));
-//            sphere(1);
-//            popMatrix();
-//        }
-        
-        
-//        sphere(100);
-//        rotateX(-0.5f);
-//        rotateY(-0.5f);
-//        fill(255, 0, 0);
-//        box(30);
-//        pushMatrix();
-//        translate(0, 0, 20);
-//        fill(0, 0, 255);
-//        box(5);
-//        popMatrix();
+        // Draw a scale bar (from https://code.google.com/p/unfolding/source/browse/trunk/unfolding/examples/de/fhpotsdam/unfolding/examples/MapWithBarScaleApp.java?r=140 )
+        drawBarScale(20, map.mapDisplay.getHeight() - 20);
+
 
     }
 
@@ -203,7 +155,7 @@ public class Main extends PApplet {
             lat = (Float.parseFloat(lineSplit[2])); // y, e.g. 53.8
             lon = (Float.parseFloat(lineSplit[3])); // x, e.g. -1.63
             PVector geo = new PVector(lon, lat);
-            latloncoords.add(geo);
+            latloncoords.add(new Location(lat,lon));
             //bngcoord = bng.transformCoords(geo);
             //bngcoords.add(bngcoord);
             times.add((float) counter++);
@@ -222,9 +174,12 @@ public class Main extends PApplet {
         System.out.println("TL and BR corners: "+tlCorner.toString()+" , "+brCorner.toString());
         
         // Second pass - work out the screen coordinates (need min/max lat lon first)
-        for (PVector geo:latloncoords) {
+        for (Location geo:latloncoords) {
                     //screenCoords.add(geoToScreen(bngcoord));
-            screenCoords.add(geoToScreen(geo));
+//            screenCoords.add(geoToScreen(geo));
+            System.out.println("GEO:" +geo);
+            screenCoords.add(map.getScreenPosition(geo));
+            System.out.println(geo+" -- " + map.getScreenPosition(geo));
             // Convert to BNG and add to lists:
 //            xcors.add(Float.parseFloat(lineSplit[2]));
 //            ycors.add(Float.parseFloat(lineSplit[3]));
@@ -234,13 +189,91 @@ public class Main extends PApplet {
 
     }
 
-    PVector geoToScreen(PVector geo) {
-        return new PVector(
-                map(geo.x, tlCorner.x, brCorner.x, 0, width),
-                map(geo.y, tlCorner.y, brCorner.y, 0, height));
-    }
+//    PVector geoToScreen(PVector geo) {
+//        return new PVector(
+//                map(geo.x, tlCorner.x, brCorner.x, 0, width),
+//                map(geo.y, tlCorner.y, brCorner.y, 0, height));
+//    }
 
     public static void main(String[] args) {
         PApplet.main(new String[]{"spacetime.Main"});
     }
+    
+    
+    
+    
+    
+    
+    // STUFF FOR A SCALE BAR, FROM: https://code.google.com/p/unfolding/source/browse/trunk/unfolding/examples/de/fhpotsdam/unfolding/examples/MapWithBarScaleApp.java?r=140
+    
+    
+    
+    private static final List<Float> DISPLAY_DISTANCES = Arrays.asList(0.01f, 0.02f, 0.05f, 0.1f, 0.2f, 0.5f, 1f, 2f,
+                        5f, 10f, 20f, 50f, 100f, 200f, 500f, 1000f, 2000f, 5000f);
+        private static final float MAX_DISPLAY_DISTANCE = 5000;
+
+    
+        /**
+         * Draws a bar scale at given position according to current zoom level.
+         *
+         * Calculates distance at equator (scale is dependent on Latitude). Uses a distance to display
+         * from fixed set of distance numbers, so length of bar may vary.
+         *
+         * @param x
+         *            Position to display bar scale
+         * @param y
+         *            Position to display bar scale
+         */
+        public void drawBarScale(float x, float y) {
+
+                // Distance in km, appropriate to current zoom
+                float distance = MAX_DISPLAY_DISTANCE / map.getZoom();
+                distance = getClosestDistance(distance);
+
+                // Gets destLocation (world center, on equator, with calculated distance)
+                Location startLocation = new Location(0, 0);
+                Location destLocation = GeoUtils.getDestinationLocation(startLocation, 90f, distance);
+
+                // Calculates distance between both locations in screen coordinates
+                float[] destXY = map.getScreenPositionFromLocation(destLocation);
+                float[] startXY = map.getScreenPositionFromLocation(startLocation);
+                float dx = destXY[0] - startXY[0];
+
+                // Display
+                stroke(30);
+                strokeWeight(1);
+                line(x, y - 3, x, y + 3);
+                line(x, y, x + dx, y);
+                line(x + dx, y - 3, x + dx, y + 3);
+                fill(30);
+                text(nfs(distance, 0, 0) + " km", x + dx + 3, y + 4);
+        }
+
+        /**
+         * Returns the nearest distance to display as well as to use for calculation.
+         *
+         * @param distance
+         *            The original distance
+         * @return A distance from the set of {@link DISPLAY_DISTANCES}
+         */
+        public float getClosestDistance(float distance) {
+                return closest(distance, DISPLAY_DISTANCES);
+        }
+
+        public float closest(float of, List<Float> in) {
+                float min = Float.MAX_VALUE;
+                float closest = of;
+
+                for (float v : in) {
+                        final float diff = Math.abs(v - of);
+
+                        if (diff < min) {
+                                min = diff;
+                                closest = v;
+                        }
+                }
+
+                return closest;
+        }
+
 }
